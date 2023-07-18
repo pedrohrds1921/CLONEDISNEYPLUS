3d6f244e39987ca07999fc297646cae44854f0ac
Pedro Rodrigues <pedro.contate@gmail.com>
2023-07-18 01:23:33 -0300

    adjustment on mobile display

--- src/main.js
+++ src/main.js
@@ -1,5 +1,33 @@
 document.addEventListener('DOMContentLoaded',function(){
     const buttons=document.querySelectorAll('[data-tab-button]')
+    const questions=document.querySelectorAll('[data-faq-question]')
+    const heroSection=document.querySelector('.hero')
+   
+
+    const heightHero=heroSection.clientHeight
+
+
+    window.addEventListener('scroll',function(){
+       const windowScrollPosition=  window.scrollY
+
+       if(windowScrollPosition<heightHero){
+        hiddenElementHeader()
+
+       }else{
+        openElementHeader()
+       }
+    })
+
+    function hiddenElementHeader(){
+        const header=document.querySelector('.header')
+        header.classList.add('header--is-hidden')
+    }
+
+    function openElementHeader(){
+        const header=document.querySelector('.header')
+        header.classList.remove('header--is-hidden')
+    }
+
 
     buttons.forEach(button=> button.addEventListener("click",
     function(e){
@@ -12,17 +40,22 @@ document.addEventListener('DOMContentLoaded',function(){
         }
     )
     )
-   
 
+
+    questions.forEach(question=> question.addEventListener("click",
+    function(e){
+        const className='faq__questions__item--is-open'
+        const fatherElment=e.target.parentNode
+        fatherElment.classList.toggle(className)
+    }
+    ))
 })
 
 function removeActiveButton(){
     const buttons=document.querySelectorAll('[data-tab-button]')
     buttons.forEach(button=> button.classList.remove('shows__tabs__button--is-active'))
-
 }
 function hideAllTabs(){
     const tabsContainer=document.querySelectorAll('[data-tab-id]')
-
     tabsContainer.forEach(tab=> tab.classList.remove('shows__list--is-active'))
 }
