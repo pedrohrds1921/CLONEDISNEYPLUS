4f6bd11a81434e99a01271aaa5e97233d746d33e
Pedro Rodrigues <pedro.contate@gmail.com>
2023-07-18 01:34:54 -0300

    config uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 const gulp=require('gulp')
 const sass=require('gulp-sass')(require('sass'))
 const imagemin=require('gulp-imagemin')
+const uglify= require('gulp-uglify')
 
 
+const scripts=()=>{
+
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/scripts'))
+}
 
 const styles=()=>{
     return gulp.src('./src/styles/*.scss')
@@ -16,10 +23,11 @@ const images=()=>{
     .pipe(imagemin())
     .pipe(gulp.dest('./dist/images'))
 }
-exports.default=gulp.parallel(styles,images)
+exports.default=gulp.parallel(styles,images,scripts)
 
 exports.watch = function(){
 
 
     gulp.watch('./src/styles/*.scss',()=>styles())
+    gulp.watch('./src/scripts/*.js',gulp.parallel(scripts))
 }
